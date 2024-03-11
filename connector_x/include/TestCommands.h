8e9554df483c9d64d5b30203c6a9a37e93d5c626
having11 <gatoninja236@gmail.com>
2024-03-11 01:07:30 -0500

    extend test pattern

--- connector_x/include/TestCommands.h
+++ connector_x/include/TestCommands.h
@@ -70,4 +70,129 @@ static std::vector<TestCommand> testCommands =
         },
         .delayMs = 500,
     },
+    {
+        .cmd = {
+            .commandType = CommandType::SetLedPort,
+            .commandData = {
+                .commandSetLedPort = {
+                    .port = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::SetPatternZone,
+            .commandData = {
+                .commandSetPatternZone = {
+                    .zoneIndex = 3,
+                    .reversed = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::ChangeColor,
+            .commandData = {
+                .commandColor = {
+                    .red = 255,
+                    .green = 255,
+                    .blue = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::Pattern,
+            .commandData = {
+                .commandPattern = {
+                    .pattern = 7,
+                    .oneShot = 0,
+                    .delay = 50,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::SetPatternZone,
+            .commandData = {
+                .commandSetPatternZone = {
+                    .zoneIndex = 1,
+                    .reversed = 1,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::ChangeColor,
+            .commandData = {
+                .commandColor = {
+                    .red = 0,
+                    .green = 255,
+                    .blue = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::Pattern,
+            .commandData = {
+                .commandPattern = {
+                    .pattern = 6,
+                    .oneShot = 0,
+                    .delay = 50,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::SetPatternZone,
+            .commandData = {
+                .commandSetPatternZone = {
+                    .zoneIndex = 0,
+                    .reversed = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::ChangeColor,
+            .commandData = {
+                .commandColor = {
+                    .red = 0,
+                    .green = 255,
+                    .blue = 0,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
+    {
+        .cmd = {
+            .commandType = CommandType::Pattern,
+            .commandData = {
+                .commandPattern = {
+                    .pattern = 3,
+                    .oneShot = 0,
+                    .delay = 20,
+                },
+            },
+        },
+        .delayMs = 500,
+    },
 };
